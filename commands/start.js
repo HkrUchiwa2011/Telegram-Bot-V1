09716e4859fb2df0c2fd547bd1449fd5394387df
HkrUchiwa2011 <fadilbouko780@gmail.com>
2024-12-29 21:10:37 +0100

    start.js

--- commands/start.js
+++ commands/start.js
@@ -2,7 +2,7 @@ module.exports = (bot) => {
   bot.command('start', (ctx) => {
     const userName = ctx.from.first_name; 
 
-    const welcomeMessage = `👋 Bonjour ${userName} \n\nBienvenue chère utilisateur ou utilisatrice moi c'est Ronald Chatbot \n\n- /help consultez la liste de mes commandes \n\n veuillez contacter @Alexandre_Ronald_SORY si le bot à un problème 🤗`;
+    const welcomeMessage = `👋 Bonjour ${userName} \n\n Bienvenue à vous,créature supérieure. Moi c'est FadilSasuChatbot \n\n- Tappez /help pour consulter la liste de mes commandes \n\n Veuillez s'il vous plaît laisser un message à @fadil_uchiwa si le bot recontre des problèmes 😇`;
 
     ctx.reply(welcomeMessage);
   });
