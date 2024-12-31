34290218f02b8b953fd8eb8b69b995bbeac3e788
HkrUchiwa2011 <fadilbouko780@gmail.com>
2024-12-31 23:31:54 +0100

    lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,27 +1,23 @@
-const { Telegraf } = require('telegraf');
 const axios = require('axios');
-const bot = new Telegraf('8097762189:AAEra_qZVKfnFE_aOhFSnd8fX2efgxFty3M');
 
-bot.command('lyrics', async (ctx) => {
-    const input = ctx.message.text.split(' ').slice(1).join(' ');
-
-    if (!input) {
-        return ctx.reply("Veuillez fournir le titre de la chanson et/ou l'artiste. Exemple : /lyrics Tiakola Meuda");
-    }
-
-    const apiKey = "c88b9be90ffcdd74976efe753db468f1";
-    const url = `https://api.lyrics.ovh/v1/${input}`;
-
-    try {
-        const response = await axios.get(url);
-        if (response.data.lyrics) {
-            ctx.reply(`Voici les paroles :\n\n${response.data.lyrics}`);
-        } else {
-            ctx.reply("Paroles introuvables. Assurez-vous que le titre ou l'artiste est correct.");
+function registerLyricsCommand(bot) {
+    bot.command('lyrics', async (ctx) => {
+        const input = ctx.message.text.split(' ').slice(1).join(' ');
+        if (!input) {
+            return ctx.reply("Veuillez fournir le titre de la chanson. Exemple : /lyrics Tiakola Meuda");
         }
-    } catch (error) {
-        ctx.reply("Erreur lors de la recherche des paroles. Veuillez réessayer.");
-    }
-});
 
-module.exports = bot;
+        try {
+            const response = await axios.get(`https://api.lyrics.ovh/v1/${input}`);
+            if (response.data.lyrics) {
+                ctx.reply(`Voici les paroles :\n\n${response.data.lyrics}`);
+            } else {
+                ctx.reply("Paroles introuvables. Assurez-vous que le titre est correct.");
+            }
+        } catch (error) {
+            ctx.reply("Erreur lors de la récupération des paroles.");
+        }
+    });
+}
+
+module.exports = registerLyricsCommand;
