5a1f5f3d6a82f522e6390badcfdb7581ca3f104c
HkrUchiwa2011 <fadilbouko780@gmail.com>
2025-01-01 09:14:25 +0100

    out.js

--- commands/out.js
+++ commands/out.js
@@ -1,7 +1,6 @@
 module.exports = (bot) => {
   bot.command('out', async (ctx) => {
     try {
-      // Vérifier si l'utilisateur est un admin dans le groupe
       if (ctx.chat.type === 'group' || ctx.chat.type === 'supergroup') {
         const admins = await ctx.getChatAdministrators();
         const isAdmin = admins.some((admin) => admin.user.id === ctx.from.id);
@@ -10,15 +9,14 @@ module.exports = (bot) => {
           return ctx.reply("🚫 Seuls les administrateurs du groupe peuvent demander au bot de sortir.");
         }
 
-        // Quitter le groupe
         await ctx.leaveChat();
         ctx.reply("👋 Le bot a quitté le groupe.");
       } else {
         ctx.reply("Cette commande est uniquement disponible dans les groupes.");
       }
     } catch (error) {
-      console.error("Erreur dans la commande /out :", error.message);
-      ctx.reply("❌ Une erreur est survenue. Le bot n'a pas pu quitter le groupe.");
+      console.error('Erreur dans la commande /out :', error.message);
+      ctx.reply('❌ Une erreur est survenue. Le bot n\'a pas pu quitter le groupe.');
     }
   });
 };
