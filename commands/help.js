c0e04d2440828899c80c8d89b5ee61cecfe31af7
HkrUchiwa2011 <fadilbouko780@gmail.com>
2025-01-01 06:11:18 +0100

    help.js

--- commands/help.js
+++ commands/help.js
@@ -15,6 +15,7 @@ module.exports = (bot) => {
             { command: '/lyrics', description: 'Retrouvez les paroles de vos chansons' },
             { command: '/out' , description : 'Permet de faire sortir le bot dun groupe'},
             { command: '/users' , description : ' Permet de voir les utilisateurs du bot'},
+            { command: '/ban' , description : ' Aide les administrateurs du groupe à bannir'},
     
        
         ];
